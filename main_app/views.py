fe217ea542c0ae86bf0e1951bd296008d7d28858
Rocky Liwanag <studiorockprod@me.com>
2020-03-21 10:55:35 -0700

    profile page

--- main_app/views.py
+++ main_app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Landing route
 def landing(request):
-    return render(request, 'registration/login.html')
+    return render(request, '../templates/registration/login.html')
 
 def home(request):
     return render(request, "main_app/home.html")
 
 def login(request):
-    return render(request, 'registration/login.html')
+    return render(request, '../templates/registration/login.html')
 
 def profile(request):
     return render(request, 'main_app/profile.html')
