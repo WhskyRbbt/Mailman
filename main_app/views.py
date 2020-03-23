6262c1e7a949657165c2a1ba9175479181c85992
WhskyRbbt <WhskyRbbt@Jonathans-Air.home>
2020-03-23 09:52:59 -0700

    link and routing completed for signup

--- main_app/views.py
+++ main_app/views.py
@@ -24,8 +24,8 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('signup')
+            login(request)
+            return redirect('/login/')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
