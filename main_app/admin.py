16e67660995d329377736c17f8ba0528bcec0e75
Ryan Branco <ryanbr23@gmail.com>
2020-03-24 21:55:45 -0700

    model changes

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin 
+# from django.contrib.gis.admin import OSMGeoAdmin 
 
 # Register your models here.
-from .models import User, Package, Profile
-admin.site.register(User)
+from .models import Package, Profile
+admin.site.register(Profile)
 admin.site.register(Package)
 
-admin.site.unregister(User)
-admin.site.unregister(Package)
+# admin.site.unregister(User)
+# admin.site.unregister(Package)
 
-@admin.register(User)
-class UserAdmin(OSMGeoAdmin):
-    display = ('display_name')
+# @admin.register(User)
+# class UserAdmin(OSMGeoAdmin):
+#     display = ('display_name')
 
-@admin.register(Package)
-class PackageAdmin(OSMGeoAdmin):
-    display = ('origination')
+# @admin.register(Package)
+# class PackageAdmin(OSMGeoAdmin):
+#     display = ('origination')
 
-@admin.register(Profile)
-class ProfileAdmin(OSMGeoAdmin):
-    display = ('user')
+# @admin.register(Profile)
+# class ProfileAdmin(OSMGeoAdmin):
+#     display = ('user')
--- main_app/models.py
+++ main_app/models.py
@@ -8,15 +8,14 @@ from django.urls import reverse
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class User(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    display_name = models.CharField(max_length=15)
-    email = models.CharField(max_length=75)
-    password = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.first_name
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.gis.db import models
+from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Package(models.Model):
     origination = models.CharField(max_length=100)
@@ -26,19 +25,20 @@ class Package(models.Model):
     height = models.IntegerField()
     weight = models.IntegerField()
     is_fragile = models.BooleanField()
-    destination_length = models.IntegerField()
-    cost_of_delivery = models.IntegerField()
-    completed = models.BooleanField(default=False)
+    destination_length = models.IntegerField(blank=True, null=True)
+    cost_of_delivery = models.IntegerField(blank=True, null=True)
+    completed = models.BooleanField()
     users = models.ManyToManyField(User)
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pkg_id': self.id})
-    
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    deliveries = models.ForeignKey(Package, on_delete=models.CASCADE)
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
